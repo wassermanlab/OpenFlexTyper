32feca207d047fe2102a5e76f48f387ff64b1eb3
godfrain <54413762+godfrain@users.noreply.github.com>
2019-11-25 11:27:26 -0800

    Update and rename uninstall.sh to install.sh
    adding install.sh

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+########################################################################
+###
+### Copyright (c) 2018, Wasserman lab
+###
+### FILE        install.sh
+###
+### DESCRIPTION This is the install.sh script for the project.
+###             We use conda in order to install the application.
+###
+### Initial version @ Godfrain Jacques KOUNKOU
+###
+########################################################################
+
+# set -e
+
+function install() 
+{
+	conda config --add channels conda-forge
+	conda create --prefix ../openFlexTyper --file requirements.txt
+	conda activate ../openFlexTyper
+	mkdir build
+	cd build
+	qmake ..
+	make
+}
+
+install
