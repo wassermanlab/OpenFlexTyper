e777f5be08f252dc42f79c3a27e2866a37bc2b37
Phillip Richmond <richmonp@login03.sockeye.arc.ubc.ca>
2019-12-10 13:47:51 -0800

    Added ancestry manuscript data, as well as directories for cytoscan and svgenotyping

---
+++ manuscript/ancestry/run_Settings_African_father.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#PBS -A st-wasserww-1
+#PBS -N ancestry_test_african_father
+#PBS -V
+#PBS -M gkounkou@cmmt.ubc.ca
+#PBS -l select=1:ncpus=32:mem=100gb
+#PBS -l walltime=20:0:0
+
+export QT_QPA_PLATFORM='offscreen'
+cd $PBS_O_WORKDIR
+
+FLEX_BIN='/scratch/st-wasserww-1/FLEXTYPER/OpenFlexTyper/build'
+$FLEX_BIN/flextyper searching -c Settings_African_father.ini
---
+++ manuscript/ancestry/run_Settings_Asian_kid.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#PBS -A st-wasserww-1
+#PBS -N ancestry_test_kid
+#PBS -V
+#PBS -M gkounkou@cmmt.ubc.ca
+#PBS -l select=1:ncpus=32:mem=100gb
+#PBS -l walltime=20:0:0
+
+export QT_QPA_PLATFORM='offscreen'
+cd $PBS_O_WORKDIR
+
+FLEX_BIN='/scratch/st-wasserww-1/FLEXTYPER/OpenFlexTyper/build'
+$FLEX_BIN/flextyper searching -c Settings_Asian_kid.ini
