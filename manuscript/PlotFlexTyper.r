98f7679f551b153ecbaa2f1604a0f97d2366700b
phillip-a-richmond <phillip.a.richmond@gmail.com>
2020-01-24 20:06:02 -0800

    adding plotting for fig 3,4

--- manuscript/PlotFlexTyper.r
+++ manuscript/PlotFlexTyper.r
@@ -44,7 +44,7 @@ library(argparser)
 #argv <- parse_args(p)
 #?argparser
 
-# - Fig 3A,B - #
+# - Fig 3A - #
 
 # Make an output directory called output/
 system('mkdir -p /Users/philliprichmond/Dropbox/Grad_School/FlexTyper/OpenFlexTyper/manuscript/PlottingOutput/')
@@ -53,103 +53,118 @@ system('cd /Users/philliprichmond/Dropbox/Grad_School/FlexTyper/OpenFlexTyper/ma
 # files <- c("QUERY_FILE_SProb_21-10_20190731.tsv")
 #files <- args
 files <- c("/Users/philliprichmond/Dropbox/Grad_School/FlexTyper/OpenFlexTyper/manuscript/performance/MultiKmerSearch.tsv")
- 
+
 
 KmerData <- read.csv(file.path(files), sep='\t', header=TRUE)
 dim(KmerData)
 summarise(KmerData)
 KmerData
-KmerData$Threads <- as.numeric(as.character(KmerData$Threads))
+KmerData$Threads <- (as.character(KmerData$Threads))
 KmerData$number_of_kmers <- as.numeric(as.character(KmerData$number_of_kmers))
 KmerData$search_time <- as.numeric(as.character(KmerData$search_time))
-KmerData$kmer_size <- as.numeric(as.character(KmerData$kmer_size))
+KmerData$kmer_size <- (as.character(KmerData$kmer_size))
+
+my.theme <- theme_classic()  + theme(aspect.ratio = 1) + theme(legend.position="bottom")
+my.theme2 <-  theme_classic() + theme(aspect.ratio = 1/2) + theme(legend.position="bottom")
+
+#PlotKmers <- function(my.theme) {}
+subDF <- subset(KmerData,kmer_size %in% c("25","150") & number_of_kmers > 10)
+subDFgrouped <- group_by(subDF,Threads)
+subDFgrouped
+p_line_thread1 <- ggplot(data=subDFgrouped, aes(x = number_of_kmers, y = search_time),na.rm=TRUE) 
+p_line_thread1 + 
+  my.theme2 +
+  geom_line(aes(color=kmer_size, linetype=Threads)) +
+  geom_point(aes(color=kmer_size, shape=Threads)) +
+  scale_x_continuous(trans='log10') +
+  scale_y_continuous(trans='log10') +
+  ggsave("KmerPerformance1thread.pdf",plot= last_plot(), path="/Users/philliprichmond/Dropbox/Grad_School/FlexTyper/OpenFlexTyper/manuscript/PlottingOutput/")
+  
+# 
+# p_line_thread2 <- ggplot(data=KmerData[KmerData$Threads==30 & KmerData$number_of_kmers>=10,],aes(x = number_of_kmers, y = search_time, group = kmer_size),na.rm=TRUE,linetype='dashed') 
+# p_line_thread2 + 
+#   my.theme2 +
+#   geom_line(aes(color=kmer_size), linetype='dashed') +
+#   geom_point(aes(color=kmer_size), shape=2) +
+#   scale_x_continuous(trans='log10') +
+#   scale_y_continuous(trans='log10') +
+#   scale_color_gradient(low="blue",high="red") +
+#   ggsave("KmerPerformance30thread.pdf",plot= last_plot(), path="/Users/philliprichmond/Dropbox/Grad_School/FlexTyper/OpenFlexTyper/manuscript/PlottingOutput/")
+#   
+
+
+
+##############
+# - Fig 3B - #
+##############
+
+files2 <- c("/Users/philliprichmond/Dropbox/Grad_School/FlexTyper/OpenFlexTyper/manuscript/performance/MulthreadDemonstration.tsv")
+
+KmerThreadData <- read.csv(file.path(files2), sep='\t', header=TRUE)
+dim(KmerThreadData)
+summarise(KmerThreadData)
+KmerThreadData
+KmerThreadData$number_of_threads <- as.numeric(as.character(KmerThreadData$number_of_threads))
+KmerThreadData$number_of_kmer <- as.numeric(as.character(KmerThreadData$number_of_kmer))
+KmerThreadData$search_time <- as.numeric(as.character(KmerThreadData$search_time))
+KmerThreadData$kmer_size <- (as.character(KmerThreadData$kmer_size))
 
 my.theme <- theme_classic()  + theme(aspect.ratio = 1) + theme(legend.position="bottom")
 my.theme2 <- theme_classic()  + theme(aspect.ratio = 1/2) + theme(legend.position="bottom")
+my.theme3 <-  theme_linedraw() + theme(aspect.ratio = 1/2) + theme(legend.position="bottom")
 
 #PlotKmers <- function(my.theme) {}
+subDF <- subset(KmerThreadData,kmer_size %in% c("25") & number_of_kmer == 100000)
 
-p_line_thread1 <- ggplot(data=KmerData[KmerData$Threads==1 & KmerData$number_of_kmers>=10,],aes(x = number_of_kmers, y = search_time, group = kmer_size),na.rm=TRUE) 
-p_line_thread1 + 
+p_line_thread_by_time_kmer25 <- ggplot(data=subDF,aes(x = number_of_threads, y = search_time),na.rm=TRUE)
+p_line_thread_by_time_kmer25 +
   my.theme2 +
-  geom_path(aes(color=kmer_size)) +
+  geom_line(aes(color=kmer_size)) +
   geom_point(aes(color=kmer_size)) +
-  scale_x_continuous(trans='log10') +
-  scale_y_continuous(trans='log10') +
-  scale_color_gradient(low="blue",high="red") +
-  ggsave("KmerPerformance1thread.pdf",plot= last_plot(), path="/Users/philliprichmond/Dropbox/Grad_School/FlexTyper/OpenFlexTyper/manuscript/PlottingOutput/")
-  
-p_line_thread2 <- ggplot(data=KmerData[KmerData$Threads==30 & KmerData$number_of_kmers>=10,],aes(x = number_of_kmers, y = search_time, group = kmer_size,linetype='dashed'),na.rm=TRUE) 
-p_line_thread2 + 
-  my.theme2 +
-  geom_line(aes(color=kmer_size), linetype='dashed') +
-  geom_point(aes(color=kmer_size), shape=2) +
-  scale_x_continuous(trans='log10') +
-  scale_y_continuous(trans='log10') +
-  scale_color_gradient(low="blue",high="red") +
-  ggsave("KmerPerformance30thread.pdf",plot= last_plot(), path="/Users/philliprichmond/Dropbox/Grad_School/FlexTyper/OpenFlexTyper/manuscript/PlottingOutput/")
-  
-
-# This doesn't work right now
-p_line_thread_combined <- ggplot(data=KmerData[KmerData$number_of_kmers>=10,],aes(x = number_of_kmers, y = search_time, color=kmer_size, group=kmer_size, linetype=factor(KmerData$Threads),na.rm=TRUE)) 
-p_line_thread_combined + 
-  my.theme2 +
-  geom_path(aes(color=kmer_size)) +
-  geom_point(aes(color=kmer_size)) +
-  scale_x_continuous(trans='log10') +
-  scale_y_continuous(trans='log10') +
-  scale_color_gradient(low="blue",high="red") +
-  ggsave("KmerPerformanceBoththreads.pdf",plot= last_plot(), path="/Users/philliprichmond/Dropbox/Grad_School/FlexTyper/OpenFlexTyper/manuscript/PlottingOutput/")
-  
-  
+  ylim(0,8100) +
+  xlim(1, 32) +
+  scale_x_continuous(breaks = subDF$number_of_threads) +
+  ggsave("KmerPerformancekmer25.pdf",plot= last_plot(), path="/Users/philliprichmond/Dropbox/Grad_School/FlexTyper/OpenFlexTyper/manuscript/PlottingOutput/")
 
 
-# - Fig 3C - #
-
-files <- c("/Users/philliprichmond/Dropbox/Grad_School/FlexTyper/OpenFlexTyper/manuscript/performance/MultiKmerSearch.tsv")
-
+################
+# Fig 3B Try 2 #
+################
+files <- c("/Users/philliprichmond/Dropbox/Grad_School/FlexTyper/OpenFlexTyper/manuscript/performance/Multithread_Sub25_Deltas.tsv")
 
 KmerThreadData <- read.csv(file.path(files), sep='\t', header=TRUE)
-dim(KmerData)
-summarise(KmerData)
+dim(KmerThreadData)
+summarise(KmerThreadData)
 KmerThreadData
-KmerThreadData$number_of_threads <- as.numeric(as.character(KmerData$number_of_threads))
-KmerThreadData$number_of_kmers <- as.numeric(as.character(KmerData$number_of_kmers))
-KmerData$search_time <- as.numeric(as.character(KmerData$search_time))
-KmerData$kmer_size <- as.numeric(as.character(KmerData$kmer_size))
+KmerThreadData$Threads <- as.numeric(as.character(KmerThreadData$Threads))
+KmerThreadData$Expected <- as.numeric(as.character(KmerThreadData$Expected))
+KmerThreadData$Actual <- as.numeric(as.character(KmerThreadData$Actual))
+KmerThreadData$Delta <- as.numeric(as.character(KmerThreadData$Delta))
 
-my.theme <- theme_classic()  + theme(aspect.ratio = 1) + theme(legend.position="bottom")
-my.theme2 <- theme_classic()  + theme(aspect.ratio = 1/2) + theme(legend.position="bottom")
-
-#PlotKmers <- function(my.theme) {}
-
-p_line_thread1 <- ggplot(data=KmerData[KmerData$Threads==1 & KmerData$number_of_kmers>=10,],aes(x = number_of_kmers, y = search_time, group = kmer_size),na.rm=TRUE) 
-p_line_thread1 + 
+p_line_thread_by_time <- ggplot(data=KmerThreadData,aes(x = Threads, y = Actual),na.rm=TRUE) 
+p_line_thread_by_time + 
   my.theme2 +
-  geom_path(aes(color=kmer_size)) +
-  geom_point(aes(color=kmer_size)) +
-  scale_x_continuous(trans='log10') +
-  scale_y_continuous(trans='log10') +
-  scale_color_gradient(low="blue",high="red") +
-  ggsave("KmerPerformance1thread.pdf",plot= last_plot(), path="/Users/philliprichmond/Dropbox/Grad_School/FlexTyper/OpenFlexTyper/manuscript/PlottingOutput/")
-
-
-
-
+  geom_line(color='blue', linetype='solid') +
+  geom_point(color='blue', shape=19) +
+  xlim(0, 32) +
+  ylab('Time (s)') +
+  scale_x_continuous(breaks = c(0,KmerThreadData$Threads), expand = c(0, 0)) +
+  expand_limits(x = 0, y = c(0,8100)) +
+  scale_y_continuous(expand = c(0, 0)) +
+  coord_cartesian(clip = "off") +
+  geom_errorbar(aes(ymin=Expected, ymax=Actual, color='red')) +
+  geom_line(aes(x=Threads, y=Expected),linetype='dashed') +
+  ggsave("KmerPerformancekmer25_10000_deltas.pdf",plot= last_plot(), path="/Users/philliprichmond/Dropbox/Grad_School/FlexTyper/OpenFlexTyper/manuscript/PlottingOutput/")
 
   
-  
-
-#PlotKmers(my.theme)
 
 
+# - Fig 4 - #
 
-
-
-
+files3 <- c("/Users/philliprichmond/Dropbox/Grad_School/FlexTyper/ERR1955404_SProbe_20190926_014511_k31_W10_centered.tsv")
 
 # Read csv into data frame
-CountData <- read.csv(file.path(files), sep='\t',header=TRUE)
+CountData <- read.csv(file.path(files3), sep='\t',header=TRUE)
 dim(CountData)
 summarise(CountData)
 
@@ -177,33 +192,33 @@ CountData <- CountData[order(CountData$Chrom, CountData$Pos),]
 # Begin to plot, set some stuff up
 my.theme <- theme_classic()  + theme(aspect.ratio = 1) + theme(legend.position="bottom")
 my.theme2 <- theme_classic()  + theme(aspect.ratio = 1/4) + theme(legend.position="bottom")
-coverageBamFlexTyperRef <- function(my.theme) {
-    # Make basic plot as p, comparing ref coverage from bam and ref_count from FlexTyper
-    p_ref_scatter <- ggplot(data=CountData,aes(x = RefCoverageFromBam, y = Ref_count),na.rm=TRUE)
-    p_ref_scatter         + 
-      my.theme          + 
-      xlim(1,100)       + 
-      ylim(1,100)       + 
-      geom_point(alpha=0.01,size=3,shape=20) + 
-      geom_abline(linetype=2,slope=1, intercept=0, show.legend = TRUE,color='#E41A1C') +
-      xlab('Reference Coverage From Bam') +
-      ylab('Reference Count From FlexTyper')
-    ggsave("RefCoverageScatter.png",plot= last_plot(), path="output/")
-}
-
-coverageBamFlexTyperAlt <- function(my.theme) {
-	# Repeat for alt coverage
-	p_alt_scatter <- ggplot(data=CountData,aes(x = AltCoverageFromBam, y = Alt_count),na.rm=TRUE)
-	p_alt_scatter     + 
-	  my.theme          +
-  	xlim(1,100)       + 
-	  ylim(1,100)       +
-	  geom_point(alpha=0.01,size=3,shape=20) +
-	  geom_abline(linetype=2,slope=1, intercept=0, show.legend = TRUE,color='#E41A1C') +
-	  xlab('Alternate Coverage From Bam') +
-	  ylab('Alternate Count From FlexTyper')
-	ggsave("AltCoverageScatter.png",plot= last_plot(),path="output/")
-}
+# coverageBamFlexTyperRef <- function(my.theme) {
+#     # Make basic plot as p, comparing ref coverage from bam and ref_count from FlexTyper
+#     p_ref_scatter <- ggplot(data=CountData,aes(x = RefCoverageFromBam, y = Ref_count),na.rm=TRUE)
+#     p_ref_scatter         + 
+#       my.theme          + 
+#       xlim(1,100)       + 
+#       ylim(1,100)       + 
+#       geom_point(alpha=0.01,size=3,shape=20) + 
+#       geom_abline(linetype=2,slope=1, intercept=0, show.legend = TRUE,color='#E41A1C') +
+#       xlab('Reference Coverage From Bam') +
+#       ylab('Reference Count From FlexTyper')
+#     ggsave("RefCoverageScatter.pdf",plot= last_plot(), path="/Users/philliprichmond/Dropbox/Grad_School/FlexTyper/OpenFlexTyper/manuscript/PlottingOutput/")
+# }
+# 
+# coverageBamFlexTyperAlt <- function(my.theme) {
+# 	# Repeat for alt coverage
+# 	p_alt_scatter <- ggplot(data=CountData,aes(x = AltCoverageFromBam, y = Alt_count),na.rm=TRUE)
+# 	p_alt_scatter     + 
+# 	  my.theme          +
+#   	xlim(1,100)       + 
+# 	  ylim(1,100)       +
+# 	  geom_point(alpha=0.01,size=3,shape=20) +
+# 	  geom_abline(linetype=2,slope=1, intercept=0, show.legend = TRUE,color='#E41A1C') +
+# 	  xlab('Alternate Coverage From Bam') +
+# 	  ylab('Alternate Count From FlexTyper')
+# 	ggsave("AltCoverageScatter.pdf",plot= last_plot(),path="/Users/philliprichmond/Dropbox/Grad_School/FlexTyper/OpenFlexTyper/manuscript/PlottingOutput/")
+# }
 
 totalCoverageScatterBamFlexTyper <- function(my.theme) {
 	# And total coverage
@@ -212,11 +227,12 @@ totalCoverageScatterBamFlexTyper <- function(my.theme) {
 	  my.theme 	  +
 	  xlim(1,100) 	  + 
 	  ylim(1,100) 	  +
-	  geom_point(alpha=0.01,size=3,shape=20) +
+	  geom_bin2d(bins=100) + 
+	  scale_fill_gradient(low="lightblue1",high="darkblue",trans="log10") + 
 	  geom_abline(linetype=2,slope=1, intercept=0, show.legend = TRUE,color='#E41A1C') +
 	  xlab('Total Coverage From Bam') +
 	  ylab('Total Count From FlexTyper')
-	ggsave("TotalCoverageScatter.png",plot= last_plot(), path="output/")
+	ggsave("TotalCoverageScatter.pdf",plot= last_plot(), path="/Users/philliprichmond/Dropbox/Grad_School/FlexTyper/OpenFlexTyper/manuscript/PlottingOutput/")
 }
 
 posAlongXAxisTotalFlexTyperonY <- function(my.theme) {
@@ -230,7 +246,7 @@ posAlongXAxisTotalFlexTyperonY <- function(my.theme) {
 	  geom_point(aes(x = Pos, y = TotalCoverageFromBam, color="BAM" ),alpha=0.1) +
 	  xlab('Chromsomal Position [chromosome 1]')
 	  ylab('Total Coverage and Total Count')
-	ggsave("TotalPositionalBoth.png",plot= last_plot(), height=5, width=15, path="output/")
+	ggsave("TotalPositionalBoth.pdf",plot= last_plot(), height=5, width=15, path="/Users/philliprichmond/Dropbox/Grad_School/FlexTyper/OpenFlexTyper/manuscript/PlottingOutput/")
 }
 
 deltasAlongPosAxisTot <- function(my.theme) {
@@ -242,43 +258,43 @@ deltasAlongPosAxisTot <- function(my.theme) {
 	  geom_point(alpha=0.05) +
 	  geom_abline(linetype=2,slope=0,intercept=0,color='black') +
 	  xlab('Chromsomal Position [chromosome 1]')
-	ggsave("DeltaTotalPositional.png",plot= last_plot(), height=5, width=15, path="output/")
+	ggsave("DeltaTotalPositional.pdf",plot= last_plot(), height=5, width=15, path="/Users/philliprichmond/Dropbox/Grad_School/FlexTyper/OpenFlexTyper/manuscript/PlottingOutput/")
 }
 
-deltaAlongPosAxisAlt <- function(my.theme) {
-	# Plot the delta along the positional axis, centered on 0 for y 
-	p_pos_delta_alt <- ggplot(data=CountData[CountData$Chrom=='1',], aes(x=Pos, y=DeltaAlt))
-	p_pos_delta_alt  + 
-	  my.theme 	 +
-	  ylim(-50,50) 	 +
-	  geom_point(alpha=0.05,color='red') +
-	  geom_abline(linetype=2,slope=0,intercept=0,color='red') +
-	  xlab('Chromsomal Position [chromosome 1]')
-	ggsave("DeltaAltPositional.png",plot= last_plot(), height=5, width=15, path="output/")
-}
+# deltaAlongPosAxisAlt <- function(my.theme) {
+# 	# Plot the delta along the positional axis, centered on 0 for y 
+# 	p_pos_delta_alt <- ggplot(data=CountData[CountData$Chrom=='1',], aes(x=Pos, y=DeltaAlt))
+# 	p_pos_delta_alt  + 
+# 	  my.theme 	 +
+# 	  ylim(-50,50) 	 +
+# 	  geom_point(alpha=0.05,color='red') +
+# 	  geom_abline(linetype=2,slope=0,intercept=0,color='red') +
+# 	  xlab('Chromsomal Position [chromosome 1]')
+# 	ggsave("DeltaAltPositional.pdf",plot= last_plot(), height=5, width=15, path="/Users/philliprichmond/Dropbox/Grad_School/FlexTyper/OpenFlexTyper/manuscript/PlottingOutput/")
+# }
+# 
+# deltaAlongPosAxisRef <- function(my.theme) {
+# 	# Plot the delta along the positional axis, centered on 0 for y 
+# 	p_pos_delta_ref <- ggplot(data=CountData[CountData$Chrom=='1',], mapping=aes(x=Pos, y=DeltaRef))
+# 	p_pos_delta_ref  + 
+# 	  my.theme 	 +
+# 	  ylim(-50,50) 	 +
+# 	  geom_point(alpha=0.05,color='blue') +
+# 	  geom_abline(linetype=2,slope=0,intercept=0,color='red') +
+# 	  xlab('Chromsomal Position [chromosome 1]')
+# 	ggsave("DeltaRefPositional.pdf",plot= last_plot(), height=5, width=15, path="/Users/philliprichmond/Dropbox/Grad_School/FlexTyper/OpenFlexTyper/manuscript/PlottingOutput/")
+# }
 
-deltaAlongPosAxisRef <- function(my.theme) {
-	# Plot the delta along the positional axis, centered on 0 for y 
-	p_pos_delta_ref <- ggplot(data=CountData[CountData$Chrom=='1',], mapping=aes(x=Pos, y=DeltaRef))
-	p_pos_delta_ref  + 
-	  my.theme 	 +
-	  ylim(-50,50) 	 +
-	  geom_point(alpha=0.05,color='blue') +
-	  geom_abline(linetype=2,slope=0,intercept=0,color='red') +
-	  xlab('Chromsomal Position [chromosome 1]')
-	ggsave("DeltaRefPositional.png",plot= last_plot(), height=5, width=15, path="output/")
-}
-
-deltas_histogram_ref <- function(my.theme) {
-	# Plot a histogram of the deltas
-	p_delta_hist <- ggplot(data=CountData)
-	p_delta_hist 	 +
-	  my.theme 	 + 
-	  xlim(-50,50) 	 +
-	  geom_histogram(binwidth=2,aes(DeltaRef,color="Reference"),alpha=0.05) +
-	  xlab('Delta between FlexTyper - BamCoverage')
-	ggsave("DeltaHistBoth.png",plot= last_plot(), path="output/")
-}
+# deltas_histogram_ref <- function(my.theme) {
+# 	# Plot a histogram of the deltas
+# 	p_delta_hist <- ggplot(data=CountData)
+# 	p_delta_hist 	 +
+# 	  my.theme 	 + 
+# 	  xlim(-50,50) 	 +
+# 	  geom_histogram(binwidth=2,aes(DeltaRef,color="Reference"),alpha=0.05) +
+# 	  xlab('Delta between FlexTyper - BamCoverage')
+# 	ggsave("DeltaHistBoth.pdf",plot= last_plot(), path="/Users/philliprichmond/Dropbox/Grad_School/FlexTyper/OpenFlexTyper/manuscript/PlottingOutput/")
+# }
 
 deltas_histogram_both <- function(my.theme) {
 	# Plot a histogram of the deltas
@@ -289,19 +305,19 @@ deltas_histogram_both <- function(my.theme) {
 	  geom_histogram(binwidth=2,aes(DeltaRef,color="Reference"),alpha=0.05) +
 	  geom_histogram(binwidth=2,aes(DeltaAlt,color="Alternate"),alpha=0.05) +
 	  xlab('Delta between FlexTyper - BamCoverage')
-	ggsave("DeltaHistBoth.png",plot= last_plot(), path="output/")
+	ggsave("DeltaHistBoth.pdf",plot= last_plot(), path="/Users/philliprichmond/Dropbox/Grad_School/FlexTyper/OpenFlexTyper/manuscript/PlottingOutput/")
 }
 
 
 
 
 # __main__ calls
-coverageBamFlexTyperRef(my.theme)
-coverageBamFlexTyperAlt(my.theme)
+#coverageBamFlexTyperRef(my.theme)
+#coverageBamFlexTyperAlt(my.theme)
 totalCoverageScatterBamFlexTyper(my.theme)
 posAlongXAxisTotalFlexTyperonY(my.theme2)
 deltasAlongPosAxisTot(my.theme2)
-deltaAlongPosAxisRef(my.theme2)
-deltas_histogram_ref(my.theme)
+#deltaAlongPosAxisRef(my.theme2)
+#deltas_histogram_ref(my.theme)
 deltas_histogram_both(my.theme)
-deltaAlongPosAxisAlt(my.theme2)
+#deltaAlongPosAxisAlt(my.theme2)
