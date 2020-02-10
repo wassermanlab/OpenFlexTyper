3c1b1dc65bed511b6140a43dbbc2aee7fc17bc0c
Godfrain Jacques <gkounkou@gmail.com>
2020-02-10 11:57:03 -0800

    Update main.cpp
    update version to 0.2

--- flextyper/src/main.cpp
+++ flextyper/src/main.cpp
@@ -53,7 +53,7 @@ int main(int argc, char** argv)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QGuiApplication aps(argc, argv);
     QCoreApplication::setApplicationName("flextyper");
-    QCoreApplication::setApplicationVersion("version 1.0");
+    QCoreApplication::setApplicationVersion("version 0.2");
     QCommandLineParser parser;
 
     if (argc <= 2) {
