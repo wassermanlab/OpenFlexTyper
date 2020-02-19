05c44ddce77f887a5c9ac222fb4b1e1408717299
Godfrain Jacques <gkounkou@gmail.com>
2020-02-19 11:13:18 -0800

    Update template.sh
    added comment for the type of configuration used

--- extras/testinfra/template.sh
+++ extras/testinfra/template.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+## Sockeye configuration header
 #PBS -A xxxxx
 #PBS -N test_name
 #PBS -V
@@ -7,6 +8,7 @@
 #PBS -l select=1:ncpus=32:mem=250gb
 #PBS -l walltime=24:0:0
 
+## Cedar configuration header
 ##SBATCH --account=xxxxx
 ##SBATCH --time=10:00:00
 ##SBATCH --mem-per-cpu=100G
