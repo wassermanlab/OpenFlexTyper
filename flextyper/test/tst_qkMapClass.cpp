6eb099d4b41e7ac438a6421af0af0ab52ba1f160
Unknown <akaye@cmmt.ubc.ca>
2020-07-21 11:45:29 -0700

    not sure if this is how you test a function with pointers

--- flextyper/test/tst_qkMapClass.cpp
+++ flextyper/test/tst_qkMapClass.cpp
@@ -27,18 +27,18 @@ TEST_F(TestQKMap, TestCheckForQuery)
     TEST_DESCRIPTION("check For Query");
     //bool checkForQuery(const ft::QueryClass& query) const;
     ft::QKMap _qkMap;
-    ft::QueryClass trueQuery(1, ft::QueryType::REF);
-    ft::QueryClass falseQuery(1, ft::QueryType::ALT);
-    ft::QueryClass trueQuery2(2, ft::QueryType::ALT);
-    ft::QueryClass falseQuery2(2, ft::QueryType::REF);
+    ft::QueryClass* trueQuery = new ft::QueryClass(1, ft::QueryType::REF);
+    ft::QueryClass* falseQuery = new ft::QueryClass(1, ft::QueryType::ALT);
+    ft::QueryClass* trueQuery2 = new ft::QueryClass(2, ft::QueryType::ALT);
+    ft::QueryClass* falseQuery2 = new ft::QueryClass(2, ft::QueryType::REF);
     std::set<KmerClass*> kmerSet = {new ft::KmerClass("AAAA"), new ft::KmerClass("CCCC")};
-    _qkMap._map[&trueQuery] = kmerSet;
-    _qkMap._map[&trueQuery2] = kmerSet;
+    _qkMap._map[trueQuery] = kmerSet;
+    _qkMap._map[trueQuery2] = kmerSet;
 
-    EXPECT_TRUE(_qkMap.checkForQuery(trueQuery));
-    EXPECT_TRUE(_qkMap.checkForQuery(trueQuery2));
-    EXPECT_FALSE(_qkMap.checkForQuery(falseQuery));
-    EXPECT_FALSE(_qkMap.checkForQuery(falseQuery2));
+    EXPECT_TRUE(_qkMap.checkForQuery(*trueQuery));
+    EXPECT_TRUE(_qkMap.checkForQuery(*trueQuery2));
+    EXPECT_FALSE(_qkMap.checkForQuery(*falseQuery));
+    EXPECT_FALSE(_qkMap.checkForQuery(*falseQuery2));
 
 }
 //======================================================================
@@ -48,15 +48,15 @@ TEST_F(TestQKMap, TestGetQueryPointer)
     //ft::QueryClass* getQueryPointer(const ft::QueryClass& query);
 
 
-
-
-
 }
 //======================================================================
 TEST_F(TestQKMap, TestRetrieveQueries)
 {
     TEST_DESCRIPTION("retrieve Queries");
     //std::set<ft::QueryClass*> retrieveQueries(const ft::KmerClass& kmer);
+
+
+
 
 }
 //======================================================================
@@ -71,6 +71,29 @@ TEST_F(TestQKMap, TestCheckForMatch)
 {
     TEST_DESCRIPTION("check For Match ");
     //bool checkForMatch(const ft::QueryClass& query, const ft::KmerClass &kmer) const;
+    ft::QKMap _qkMap;
+
+    ft::QueryClass* query = new ft::QueryClass(1, ft::QueryType::REF);
+    ft::QueryClass* query2 = new ft::QueryClass(1, ft::QueryType::ALT);
+    ft::QueryClass* query3 = new ft::QueryClass(2, ft::QueryType::REF);
+    ft::KmerClass* kmer = new ft::KmerClass("AAAA");
+    ft::KmerClass* kmer2 = new ft::KmerClass("CCCC");
+    ft::KmerClass* kmer3 = new ft::KmerClass("GGGG");
+    std::set<KmerClass*> kmerSet = {kmer, kmer2};
+    _qkMap._map[query] = kmerSet;
+    _qkMap._map[query2] = kmerSet;
+    _qkMap._map[query3] = kmerSet;
+
+    EXPECT_TRUE(_qkMap.checkForMatch(*query, *kmer));
+    EXPECT_TRUE(_qkMap.checkForMatch(*query, *kmer2));
+    EXPECT_FALSE(_qkMap.checkForMatch(*query, *kmer3));
+    EXPECT_TRUE(_qkMap.checkForMatch(*query2, *kmer));
+    EXPECT_TRUE(_qkMap.checkForMatch(*query2, *kmer2));
+    EXPECT_FALSE(_qkMap.checkForMatch(*query2, *kmer3));
+    EXPECT_TRUE(_qkMap.checkForMatch(*query3, *kmer));
+    EXPECT_TRUE(_qkMap.checkForMatch(*query3, *kmer2));
+    EXPECT_FALSE(_qkMap.checkForMatch(*query3, *kmer3));
+
 }
 //======================================================================
 TEST_F(TestQKMap, TestAddQKPair)
