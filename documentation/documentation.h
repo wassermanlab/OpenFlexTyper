7ed0da3e62581afd74b688dc7b54dd8d8612d81a
Unknown <akaye@cmmt.ubc.ca>
2020-10-21 10:21:13 -0700

    2

--- documentation/documentation.h
+++ documentation/documentation.h
@@ -3,9 +3,9 @@
 /// \section intro_sec Introduction
 /// FlexTyper is a tool which enables the creation of an FM-index of raw, next generation sequencing data to enable rapid and flexible sequence queries.
 ///
-/// - \subpage installpage "Installation"
-/// - \subpage search "Example: Search"
-/// - \subpage indexing "Example: Index Generation"
+/// - \ref installpage "Installation"
+/// - \ref search "Example: Search"
+/// - \ref indexing "Example: Index Generation"
 ///
 ///
 /// \copyright Copyright (c) 2020, Wasserman lab
